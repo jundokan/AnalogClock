2f8abc517353170f1e4d487f7c8245b62e017a59
Sergii Kuzmin <jundokan2000@gmail.com>
2015-10-05 03:29:25 +0300

    Inicial Commit

---
+++ AnalogClock/AnalogClock/AnalogClock.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AnalogClock.WinPhone.Resources;
+
+namespace AnalogClock.WinPhone
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
